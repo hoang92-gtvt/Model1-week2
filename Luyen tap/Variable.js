3145479be9233fb5f2f2fc4bda2211375d2d7736
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-03-07 15:37:48 +0700

    Use of variable

---
+++ Luyen tap/Variable.js
@@ -0,0 +1,23 @@
+var i=10;
+var f=20.5;
+var b= true;
+let s="Hà Nội"
+
+document.write("i=" +i);
+document.write("<br/>");
+document.write("f=" +f);
+document.write("<br/>");
+document.write("b=" +b);
+document.write("<br/>");
+document.write("s=" +s);
+
+document.write("<br/>");
+document.write("<br/>");
+document.write("<br/>");
+
+let width= 20;
+let height=10;
+let area= width *height;
+document.write("Chiều rộng hcn là " + width + "<br>");
+document.write("Chiều cao hcn là  " + height + "<br>");
+document.write("Diện tích hình chữ nhật là " + area);
