db1a6508bcc08ef5043f68a0f01a9142c41b0369
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-03-07 16:08:54 +0700

    Operator

---
+++ Luyen tap/Operator.js
@@ -0,0 +1,12 @@
+var inputwidth;
+var inputheight;
+var area;
+
+inputwidth = prompt("Nhập chiều dài hcn");
+inputheight = prompt("Nhập chiều rộng hcn");
+
+area=inputheight *inputwidth;
+
+document.write("Diện tích hình chữ nhật là: " + area);
+
+
