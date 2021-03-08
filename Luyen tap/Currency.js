1ca74c711d185b27d79e3d89981c01054322f860
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-03-08 16:25:03 +0700

    fix_currency_1

--- Luyen tap/Currency.js
+++ Luyen tap/Currency.js
@@ -11,29 +11,25 @@ function convert(){
     crr_2=document.getElementById("crr_2").value;
     alert(crr_1);
     alert(crr_2);
-    if(crr_1== "VND") {
+    if(crr_1== "VND"){
         ts_1 = 1;
-    }
-    if(crr_1== "USD"){
+    }else
+        if(crr_1== "USD"){
             ts_1=23000;
-        }
-        else
+        }else
             ts_1=22000;
 
    alert(ts_1);
 
 
-    if(crr_2== "VND") {
+    if(crr_2== "VND"){
         ts_2=1;
-    }
-    if(crr_2=="USD"){
+    }else
+        if(crr_2=="USD"){
             ts_2=23000;
-        }
-        else
+        }else
             ts_2=22000;
-
     alert(ts_2);
-
 
     var ketqua= amount + crr_1 +"có giá trị" + amount*(ts_1/ts_2) + crr_2;
 
